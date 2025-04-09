b2c1f208aed32b0bdd8e8f67d91e2ecfefec505a
islomnorbekov <islomnorbekov1319@gmail.com>
2025-04-09 11:19:37 +0500

    i added images to model and new blog post

--- second_django_project/blog/views.py
+++ second_django_project/blog/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 
 
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body', 'images']
